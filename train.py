aa5009c3b52b2e21ecfd6eca73b9d56b4eba4fe7
Yasas Sri <ezioelijahhanaang@gmail.com>
2026-03-02 23:44:02 +0530

    model promotion logic mlflow

--- train.py
+++ train.py
@@ -1,24 +1,46 @@
 import mlflow
 import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from preprocessing import load_split_data
+from mlflow_config import setup_mlflow
 
-mlflow.set_experiment("MLops-Demo")
+def train():
+    setup_mlflow()
+    X_train, X_test, y_train, y_test = load_split_data()
 
-X_train, X_test, y_train, y_test = load_split_data()
 
-model = LogisticRegression(max_iter=5000)
+    with mlflow.start_run() as run:
+        run_id = run.info.run_id
+        
+        model = LogisticRegression(max_iter=5000)
+        mlflow.log_param("model_type", "LogisticRegression")
+        
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
 
-with mlflow.start_run():
-    model.fit(X_train, y_train)
-    preds = model.predict(X_test)
+        
+        metrics = {
+            "accuracy": accuracy_score(y_test, preds),
+            "precision": precision_score(y_test, preds),
+            "recall": recall_score(y_test, preds),
+            "f1_score": f1_score(y_test, preds)
+        }
+        mlflow.log_metrics(metrics)
 
-    acc = accuracy_score(y_test, preds)
 
-    mlflow.log_param("model_type", "LogisticRegression")
-    mlflow.log_metric("accuracy", acc)
+        
 
-    mlflow.sklearn.log_model(model, "model")
+        model_name = "BreastCancerClassifier"
 
-    print("Training complete. Accuracy:", acc)
+        
+        mlflow.sklearn.log_model(model, name="model",    registered_model_name=model_name)
+
+        print(f"Model registered as: {model_name}")
+        
+        
+        print(f"Training complete. Run ID: {run_id}")
+        return run_id
+
+if __name__ == "__main__":
+    train()
